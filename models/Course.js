e75d19374a9e06769ba5c59e34e157cc27fae522
CHEN Datao <chendatao1126@gmail.com>
2019-01-13 21:27:22 +0100

    Create Course.js

---
+++ models/Course.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const courseSchema = new Schema({
+    courseNameLoc:{
+        type:String,
+        required:true
+    },
+    courseNameEn:{
+        type:String,
+        required:true
+    },
+    courseCategory:{
+        type:String,
+        required:true
+    }
+    
+
+})
+module.exports = Course = mongoose.model('Course', courseSchema);
